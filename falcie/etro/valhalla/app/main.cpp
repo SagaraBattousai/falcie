6dea1f9d9536f0253fb0cc6099b4de5dc10083c1
James Calo <jamesafcalo@gmail.com>
2023-10-25 18:32:12 +0100

    Doesn't look like this works ;(

--- falcie/etro/valhalla/app/main.cpp
+++ falcie/etro/valhalla/app/main.cpp
@@ -1,29 +1,73 @@
 
+#include <tensorflow/lite/interpreter.h>
+#include <tensorflow/lite/interpreter_builder.h>
+#include <tensorflow/lite/kernels/register.h>
+#include <tensorflow/lite/model.h>
+#include <tensorflow/lite/signature_runner.h>
+
+#include <memory>
+#include <iostream>
+
+int main() {
+  auto model = tflite::FlatBufferModel::BuildFromFile(
+      "data/trained_models/1epochs/model.tflite");
+
+  tflite::ops::builtin::BuiltinOpResolver resolver;
+
+  tflite::InterpreterBuilder builder(*model,
+                                     resolver);
+
+  if (model == nullptr) {
+    return 777;
+  }
+  std::unique_ptr<tflite::Interpreter> interpreter;
+
+  if (builder(&interpreter) != kTfLiteOk) {
+    throw std::runtime_error("Interpreter could not be build!");
+  }
+
+  tflite::SignatureRunner* runner = interpreter->GetSignatureRunner("patch");
+
+  std::cout << runner->input_size() << std::endl;
+
+  auto ten = runner->input_tensor("images");
+
+  std::cout << ten->dims->data[0] << ", " << ten->dims->data[1] << ", " << ten->dims->data[2]
+            << ", " << ten->dims->data[3]
+            << std::endl;
+
+  runner->ResizeInputTensor("images", {1, 48, 48, 3});
+
+  if (runner->AllocateTensors() != kTfLiteOk) {
+    return 777;
+  }
+  //auto input = runner->typed_tensor<float>(0);
+  auto input = runner->input_tensor("images")->data.f;
+  float x = input[0];
+  std::cout << x << std::endl;
+  
+  runner->Invoke();
+
+  //auto output = interpreter
+
+  return 0;
+}
+
+/*
 #include <valhalla/model.h>
 #include <valhalla/model_function.h>
 #include <valhalla/status_flag.h>
 #include <valhalla/tensor.h>
 
-
-#include <iostream>
-#include <vector>
-#include <memory>
-
 #include <cstdio>
+#include <iostream>
+#include <memory>
+#include <vector>
 
 int main() {
 
-  //valhalla::Model m("data/trained_models/1epochs/model.tflite", 4);
-  auto file = fopen("data/trained_models/1epochs/model.tflite", "rb");
-  fseek(file, 0, SEEK_END);
-  int flen = (int)ftell(file);
-  std::cout << "File length: " << flen << std::endl;
-  fseek(file, 0, SEEK_SET);
-  char *buffer = new char[flen];
-  fread(buffer, 1, flen, file);
-  valhalla::Model m(buffer, flen, 4);
+  valhalla::Model m("data/trained_models/1epochs/model.tflite", 4);
 
-  
   std::unique_ptr<valhalla::ModelFunction> func = m.GetSignatureRunner("patch");
   //("patch");
   int x[4] = {1, 48, 48, 3};
@@ -46,7 +90,8 @@ int main() {
   input[0] = 7.f;
 
   //sf = tensor->CopyFromBuffer(input, (48 * 48 * 3 * (int)sizeof(float)));
-  sf = tensor->CopyFromBuffer(input.data(), ((int)input.size() * (int)sizeof(float)));
+  sf = tensor->CopyFromBuffer(input.data(), ((int)input.size() *
+(int)sizeof(float)));
 
   std::cout << sf << std::endl;
 
@@ -54,7 +99,8 @@ int main() {
 
   std::cout << sf << std::endl;
 
-  std::unique_ptr<valhalla::Tensor> tensor_out = func->GetOutputTensor("output_0");
+  std::unique_ptr<valhalla::Tensor> tensor_out =
+func->GetOutputTensor("output_0");
 
   float *out = new float[1 * 64 * 108];
 
@@ -62,8 +108,8 @@ int main() {
 
   std::cout << out[0] << std::endl;
 
-  /*
-  TfLiteTensor* input_tensor =
+
+ // TfLiteTensor* input_tensor =
 ///     TfLiteInterpreterGetInputTensor(interpreter, 0);
 /// TfLiteTensorCopyFromBuffer(input_tensor, input.data(),
 ///                            input.size() * sizeof(float));
@@ -76,8 +122,10 @@ int main() {
 ///      TfLiteInterpreterGetOutputTensor(interpreter, 0);
 /// TfLiteTensorCopyToBuffer(output_tensor, output.data(),
 ///                          output.size() * sizeof(float));
-  */
+
 
 
   return 0;
 }
+
+*/
---
+++ falcie/etro/valhalla/valhalla/capi_internal/model_impl.cpp
@@ -0,0 +1,65 @@
+#include <Windows.h>
+#include <valhalla/internal/model_impl.h>
+
+#include <stdexcept>
+
+namespace valhalla {
+
+auto fd = [](TfLiteDelegate *ptr) { delete ptr; };
+
+ModelImpl::ModelImpl(const char *filename)
+    : model_(TfLiteModel_ptr(TfLiteModelCreateFromFile(filename),
+                             &TfLiteModelDelete)),
+      options_(TfLiteOptions_ptr(TfLiteInterpreterOptionsCreate(),
+                                 &TfLiteInterpreterOptionsDelete)),
+      flex_(nullptr, fd),
+      interpreter_(TfLiteInterpreter_ptr(
+          TfLiteInterpreterCreate(model_.get(), options_.get()),
+          &TfLiteInterpreterDelete)) {}
+
+ModelImpl::ModelImpl(const char *filename, int num_threads)
+    : model_(TfLiteModel_ptr(TfLiteModelCreateFromFile(filename),
+                             &TfLiteModelDelete)),
+      options_(TfLiteOptions_ptr(TfLiteInterpreterOptionsCreate(),
+                                 &TfLiteInterpreterOptionsDelete)),
+      flex_(nullptr, fd),
+      interpreter_(nullptr, &TfLiteInterpreterDelete) {
+  
+  auto hdll = LoadLibrary("tensorflowlite_flex.dll");
+
+
+
+  auto fp = reinterpret_cast<TfLiteDelegatePtr(*)()>(
+      GetProcAddress(hdll, "TF_AcquireFlexDelegate"));
+  if (fp == NULL) {
+    throw std::runtime_error("fp couldn't be run");
+  }
+
+  flex_ = fp();
+
+  TfLiteInterpreterOptionsSetNumThreads(options_.get(), num_threads);
+
+  //TfLiteInterpreterOptionsAddDelegate(options_.get(), flex_.get());
+
+  interpreter_.reset(TfLiteInterpreterCreate(model_.get(), options_.get()));
+}
+
+ModelImpl::ModelImpl(const void *model_data, int model_size, int num_threads)
+    : model_(TfLiteModel_ptr(TfLiteModelCreate(model_data, (size_t)model_size),
+                             &TfLiteModelDelete)),
+      options_(TfLiteOptions_ptr(TfLiteInterpreterOptionsCreate(),
+                                 &TfLiteInterpreterOptionsDelete)),
+      flex_(nullptr, fd),
+      interpreter_(nullptr, &TfLiteInterpreterDelete) {
+  interpreter_.reset(TfLiteInterpreterCreate(model_.get(), options_.get()));
+}
+
+ModelImpl::~ModelImpl() = default;
+
+std::unique_ptr<ModelFunctionImpl> ModelImpl::GetSignatureRunner(
+    const char *signature_name) {
+  return std::make_unique<ModelFunctionImpl>(
+      ModelFunctionImpl(interpreter_.get(), signature_name));
+}
+
+}  // namespace valhalla
